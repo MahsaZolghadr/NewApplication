ac836546ff3db85709494390f99467523be3f999
LENOVO@DESKTOP-ISOUGI7 <mahsa.er@gmail.com>
2019-08-14 16:21:00 +0430

    New Changes

---
+++ app/src/main/java/com/zolghadr/newapplication/Recycler.java
@@ -0,0 +1,14 @@
+package com.zolghadr.newapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+}
